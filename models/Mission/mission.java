8e4360f0ac6225d4d2f70ceb045ca199997700d1
dtessa <tessa.decung@ynov.com>
2026-05-05 20:47:08 +0200

    added abstract class mission and also the methods linked to it

---
+++ models/Mission/mission.java
@@ -0,0 +1,27 @@
+abstract class Mission {
+    String name;
+    boolean crewed;
+    double distance;
+    double coefficient;
+
+    public Mission(String name, boolean crewed, double distance, double coefficient) {
+        this.name = name;
+        this.crewed = crewed;
+        this.distance = distance;
+        this.coefficient = coefficient;
+    }
+
+    abstract double calculateFuel(double mass);
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isCrewedRequired() {
+        return crewed;
+    }
+
+    public double calculateRequiredFuel(Rocket rocket) {
+        return calculateFuel(rocket.calculateTotalMass());
+    }
+}
